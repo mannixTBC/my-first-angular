33c238bacd43a895992e4abf37941be3beaff13a
Teofil Badelita <43289966+mannixTBC@users.noreply.github.com>
2021-11-05 17:27:33 +0200

    Pet Dealer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {GetApiService} from './get-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-angular';
+  constructor(private api:GetApiService){
+
+  }
+  ngOnInit(){
+    this.api.apiCall().subscribe((data) => {
+      console.log('get the data' + data)
+    })
+  }
 }
